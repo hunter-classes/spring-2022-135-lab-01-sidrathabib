afed92ec71cff94498ee7bfcadc9f54957cfaf67
sidrathabib <70166676+sidrathabib@users.noreply.github.com>
2022-02-04 01:49:50 -0500

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -10,22 +10,22 @@ smaller3.cpp asks the user to input three integer numbers, and prints out the sm
 #include <iostream>
 
 int main() {
-  	//set variable and input for first number 
+  //set variable and input for first number 
 	int num1;
 	std::cout << "Enter the first number: ";
 	std::cin >> num1;
 	
-  	//set variable and input for first number 
+  //set variable and input for first number 
 	int num2;
 	std::cout << "Enter the second number: ";
 	std::cin >> num2;
 	
-  	//set variable and input for first number 
+  //set variable and input for first number 
 	int num3;
 	std::cout << "Enter the third number: ";
 	std::cin >> num3;
 	
-	//set conditions 
+  //set conditions 
 	if(num1 <= num2 && num1 <= num3)
         std::cout << "The smallest of the three is: " << num1;
 
